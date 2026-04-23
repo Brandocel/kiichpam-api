5beb0bf09e8a934d5dbf378a57c8079956e9e278
Brandocel <desarrollador@byhoka.com>
2026-04-23 16:55:00 -0500

    Fix

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,7 @@ async function bootstrap() {
   const allowedOrigins = [
     'http://127.0.0.1:3000',
     'http://localhost:3000',
+    'http://localhost:3002',
     'http://127.0.0.1:5173',
     'http://localhost:5173',
 
